4212745ff7929e4087de81b6d88636f6036fe5e5
Nico Nussmüller <nico@nussmueller.dev>
2022-10-02 21:14:36 +0200

    feat: some tests

--- FileDropFE/src/app/pages/upload/upload.component.ts
+++ FileDropFE/src/app/pages/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FileState } from 'src/app/shared/util/fileState';
 import { FileStatusEnum } from 'src/app/shared/util/fileStatusEnum';
@@ -6,25 +6,37 @@ import { FileStatusEnum } from 'src/app/shared/util/fileStatusEnum';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+  styleUrls: ['./upload.component.scss'],
 })
-export class UploadComponent{
+export class UploadComponent implements OnInit {
   fileStatusEnum = FileStatusEnum;
   uploadingFiles: Array<FileState> = new Array<FileState>();
 
-  @HostBinding('class.uploading-file') get uploadingFile() { return this.uploadingFiles.length !== 0; }
+  test = '';
 
-  constructor(
-    private router: Router
-  ) { }
+  @HostBinding('class.uploading-file') get uploadingFile() {
+    return this.uploadingFiles.length !== 0;
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.test = window.location.href;
+  }
 
   async navigateToOverview() {
-    if(this.uploadingFiles.every(x => x.status === FileStatusEnum.Uploaded)) {
+    if (
+      this.uploadingFiles.every((x) => x.status === FileStatusEnum.Uploaded)
+    ) {
       this.router.navigate(['overview']);
       return;
     }
 
-    if(confirm('You have files that are still uploading. Are you sure you want to leave this page?')) {
+    if (
+      confirm(
+        'You have files that are still uploading. Are you sure you want to leave this page?'
+      )
+    ) {
       this.router.navigate(['overview']);
     }
   }
